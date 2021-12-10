0c6eb4498e80536f553dd1ba01b65fcd726402a7
Anggit M Ginanjar <anggit@isi.co.id>
2021-12-10 21:56:59 +0700

    refactor: Send email alert if not cached. Moved send email alert section into emailCached statement

--- email.go
+++ email.go
@@ -14,28 +14,36 @@ func sendEmailAlert(containers []types.Container) {
 		panic(err)
 	}
 
-	//Initialize gomail email header and body
-	m := gomail.NewMessage()
-	m.SetHeaders(map[string][]string{
-		"From":    {m.FormatAddress(config.Email.Sender, config.Email.Name)},
-		"To":      config.Email.Recipients,
-		"Subject": {config.Email.Subject},
-	})
-
-	//Set HTML format for email body
-	emailBody, err := setEmailBody(containers, config)
+	emailCached, err := isCached(containers)
 	if err != nil {
 		panic(err)
 	}
-	m.SetBody(htmlContentType, *emailBody)
 
-	//Define gomail email client
-	d := gomail.NewDialer(config.SMTP.Address, config.SMTP.Port, config.Email.Sender, config.Email.Password)
+	//Send email alert if cache doesn't exist
+	if !emailCached {
+		//Initialize gomail email header and body
+		m := gomail.NewMessage()
+		m.SetHeaders(map[string][]string{
+			"From":    {m.FormatAddress(config.Email.Sender, config.Email.Name)},
+			"To":      config.Email.Recipients,
+			"Subject": {config.Email.Subject},
+		})
 
-	//Send email to recipients
-	informationText.Printf("[*] Sending email alert to: %v \n", config.Email.Recipients)
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+		//Set HTML format for email body
+		emailBody, err := setEmailBody(containers, config)
+		if err != nil {
+			panic(err)
+		}
+		m.SetBody(htmlContentType, *emailBody)
+
+		//Define gomail email client
+		d := gomail.NewDialer(config.SMTP.Address, config.SMTP.Port, config.Email.Sender, config.Email.Password)
+
+		//Send email to recipients
+		informationText.Printf("[*] Sending email alert to: %v \n", config.Email.Recipients)
+		if err := d.DialAndSend(m); err != nil {
+			panic(err)
+		}
 	}
 }
 
