a8c1152b05592cb7dec7ed195d4b0c762ec68227
Anggit M Ginanjar <anggit@isi.co.id>
2021-12-10 22:58:18 +0700

    feat: Make watcher run in the background with goroutine

--- watcher.go
+++ watcher.go
@@ -8,6 +8,13 @@ func runWatcher() {
 	successText.Printf("[*] --- Docker Watchdog --- \n")
 	successText.Printf("[*] Run docker containers watcher ... \n")
 
+	//Run as background with goroutine
+	go watcher()
+
+	select {}
+}
+
+func watcher() {
 	ticker := time.NewTicker(3 * time.Second)
 
 	for range ticker.C {
