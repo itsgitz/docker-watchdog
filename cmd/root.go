291f2c63abd83d40d5285e79f48d501f9f27f3a6
Anggit M Ginanjar <anggit@isi.co.id>
2021-12-15 10:12:06 +0700

    feat: Integrate with cobra cli

---
+++ cmd/root.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2021 PT Infinys System Indonesia <anggi@isi.co.id>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"os"
+
+	"docker-watchdog/watchdog"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultCfgPath = "/opt"
+	defaultCfgFile = "docker-watchdog"
+)
+
+var cfgFile string
+var parseConfig watchdog.Config
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "docker-watchdog",
+	Short: "Watch and detect all stopped docker containers",
+	Long: `Docker Watchdog is Go program that used for detects
+all stopped containers that have exited, paused, or dead state/status.
+It uses list containers endpoints from Docker Engine API.
+
+The watcher will gather all docker containers information
+repeatedly every 3 seconds using go Ticker function.
+If there are stopped containers detected, Docker Watchdog will
+send an email alert to Developer or System Administrator.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile,
+		"config",
+		"",
+		"config file (default is $HOME/.docker-watchdog.yaml)",
+	)
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(defaultCfgPath)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(defaultCfgFile)
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		watchdog.InformationText.Fprintln(os.Stderr, "[*] Using config file:", viper.ConfigFileUsed())
+	} else if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		watchdog.DangerText.Println("[!] Configuration file not found")
+		watchdog.CreateConfigurationFile(defaultCfgPath, defaultCfgFile)
+	}
+
+	//Unmarshalling the config value
+	if err := viper.Unmarshal(&parseConfig); err != nil {
+		cobra.CheckErr(err)
+	}
+}
---
+++ cmd/run.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2021 PT Infinys System Indonesia <anggit@isi.co.id>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"docker-watchdog/watchdog"
+
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run Docker Watchdog",
+	Long: `Docker Watchdog will run repeatedly to find all stopped docker containers.
+You can run this command in the background.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		//Run docker watchdog from here for `run` flag
+		wd := watchdog.Watchdog{Config: &parseConfig}
+		wd.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
