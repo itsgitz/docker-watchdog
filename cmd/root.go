904f27207488c378c7251bb2059dc303a3a88a3b
Anggit M Ginanjar <anggit@isi.co.id>
2021-12-17 11:18:41 +0700

    chore: Email typo

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 PT Infinys System Indonesia <anggi@isi.co.id>
+Copyright © 2021 PT Infinys System Indonesia <anggit@isi.co.id>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
